f8e345680501d891fe57a269d026e9727225f199
kyeongsoosoo <shakramon@naver.com>
2021-07-03 02:48:07 +0900

    Webpack / Babel / Prettier 추가

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./client/test1.js",
+  mode: "development",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://locallhost:3000/dist/",
+    hotOnly: true,
+    historyApiFallback: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin(), 
+    new HtmlWebpackPlugin({
+    template: path.join(__dirname, './client/index.html'),
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+    },
+  }),],
+};
